631331b3ef5a97c8dac38ff44067cbb6ecf04b5d
BerilOzbay <berilozbay97@gmail.com>
2024-09-19 15:27:29 +0300

    Category Entitiesi için CRUD işlemleri eklendi.

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -1,12 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Data;
+using WebApplication1.Models.Entities;
 
 namespace WebApplication1.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly TicariContext _context;
+        public CategoryController(TicariContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+           return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();  
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            var updatedCategory = _context.Categories.FirstOrDefault(c => c.Id == category.Id);
+            updatedCategory.CategoryName = category.CategoryName;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
