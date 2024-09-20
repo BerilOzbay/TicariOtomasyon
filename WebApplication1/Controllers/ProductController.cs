d75a159a32e4fc633e506def747681ac31e44527
BerilOzbay <berilozbay97@gmail.com>
2024-09-20 13:52:08 +0300

    Product Ekleme işlemi yapıldı

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Data;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly TicariContext _context;
+        public ProductController(TicariContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.Include(p => p.Category).ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
