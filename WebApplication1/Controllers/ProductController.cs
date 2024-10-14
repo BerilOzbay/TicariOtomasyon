3040c1e1714a38b334bcc21c4d18628e701bc063
BerilOzbay <berilozbay97@gmail.com>
2024-10-14 15:32:09 +0300

    Product ekleme ve silme işlemleri gerçekleştirildi.

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models.Data;
 using WebApplication1.Models.Entities;
@@ -20,14 +21,33 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IActionResult Add()
         {
+            //Dropdown liste ile kategori seçimi için ViewBag kullanılmıştır.
+            var categories = _context.Categories.ToList();
+            ViewBag.Categories = new SelectList(categories, "CategoryName", "CategoryName");
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(Product product)
         {
+            //Formdan gelen category ismi ile categori bulunmuş ve veri tabanına o şekilde eklenmiştir.
+            var category = _context.Categories.FirstOrDefault(c => c.CategoryName == product.Category.CategoryName);
+
+            product.Category = category;
             _context.Products.Add(product);
             _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id) 
+        {
+            var product = _context.Products.FirstOrDefault(c => c.Id == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
             return RedirectToAction("Index");
         }
     }
